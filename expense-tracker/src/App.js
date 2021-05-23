13d22c20b01db89d0af1b6adf905027b2952d702
shinhonggyu <zowoz0@naver.com>
2021-05-24 00:12:39 +0900

    filter 기능 만듬

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,40 +1,45 @@
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const INITIAL_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  const addExpenseData = (expense) => {
-    console.log('in App.js');
-    console.log(expense);
+const App = () => {
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevState) => {
+      const newExpenses = [expense, ...prevState];
+      return newExpenses;
+    });
   };
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseData} />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- expense-tracker/src/components/Expenses/Expenses.jsx
+++ expense-tracker/src/components/Expenses/Expenses.jsx
@@ -12,11 +12,9 @@ function Expenses({ items }) {
     setFilteredYear(selectedYear);
   };
 
-  // items.filter(item => {
-  //   if (item.date.getFullYear() === selectedYear) {
-  //     return <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
-  //   }
-  // })
+  const filteredItems = items.filter(
+    (expense) => expense.date.getFullYear() === Number(filteredYear)
+  );
 
   return (
     <Card className="expenses">
@@ -24,26 +22,14 @@ function Expenses({ items }) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      />
+      {filteredItems.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }

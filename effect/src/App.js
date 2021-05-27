ffa04033428c55168af8fe0af0d92d8bb27e56fe
shinhonggyu <zowoz0@naver.com>
2021-05-28 00:01:41 +0900

    useEffect & localStorage

--- effect/src/App.js
+++ effect/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,14 +7,26 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUser = localStorage.getItem('isLoggedIn');
+    if (storedUser === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    localStorage.setItem('isLoggedIn', '1');
+
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (

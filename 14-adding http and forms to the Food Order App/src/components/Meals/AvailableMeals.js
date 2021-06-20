f8468207d10e9ef2f8c0141c1e21d629754e4254
shinhonggyu <zowoz0@naver.com>
2021-06-20 22:48:51 +0900

    ..

--- 14-adding http and forms to the Food Order App/src/components/Meals/AvailableMeals.js
+++ 14-adding http and forms to the Food Order App/src/components/Meals/AvailableMeals.js
@@ -1,9 +1,65 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from 'react';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const resp = await fetch(
+          'https://react-http-fce6f-default-rtdb.firebaseio.com/meals.json'
+        );
+
+        if (!resp.ok) {
+          throw new Error('Something went wrong');
+        }
+
+        const responseData = await resp.json();
+
+        const loadedMeals = [];
+
+        for (const keys in responseData) {
+          loadedMeals.push({
+            id: keys,
+            name: responseData[keys].name,
+            description: responseData[keys].description,
+            price: responseData[keys].price,
+          });
+        }
+
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setHttpError(error.message);
+      }
+    };
+
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
--- 14-adding http and forms to the Food Order App/src/store/CartProvider.js
+++ 14-adding http and forms to the Food Order App/src/store/CartProvider.js
@@ -42,7 +42,7 @@ const cartReducer = (state, action) => {
     const updatedTotalAmount = state.totalAmount - existingItem.price;
     let updatedItems;
     if (existingItem.amount === 1) {
-      updatedItems = state.items.filter(item => item.id !== action.id);
+      updatedItems = state.items.filter((item) => item.id !== action.id);
     } else {
       const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
       updatedItems = [...state.items];
@@ -51,7 +51,7 @@ const cartReducer = (state, action) => {
 
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
     };
   }
 

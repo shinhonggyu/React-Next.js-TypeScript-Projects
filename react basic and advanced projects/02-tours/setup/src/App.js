a4fd4e9b76b0916da9cda4d6ab963a60dc43b698
shinhonggyu <zowoz0@naver.com>
2021-07-21 19:29:24 +0900

    up to date

---
+++ react basic and advanced projects/02-tours/setup/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project';
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const [error, setError] = useState(false);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('something went wrong!!!!');
+      }
+      const data = await response.json();
+      setIsLoading(false);
+      setTours(data);
+    } catch (error) {
+      setIsLoading(false);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (error) {
+    return (
+      <main>
+        <h1>Error!!!!!</h1>
+      </main>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;

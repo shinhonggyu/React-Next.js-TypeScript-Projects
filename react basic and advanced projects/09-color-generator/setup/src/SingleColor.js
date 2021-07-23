af9c3c3b2664d1ab941b51f4e8a3bc3d9520054c
shinhonggyu <zowoz0@naver.com>
2021-07-24 01:04:34 +0900

    up to date

--- react basic and advanced projects/09-color-generator/setup/src/SingleColor.js
+++ react basic and advanced projects/09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ index, rgb, weight, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  // console.log(bcg) : 255,255,255
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
 
-export default SingleColor
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'} `}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
